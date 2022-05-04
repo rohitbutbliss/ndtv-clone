c7ac6cd2a93f5d844eece616eeb3393effcf6892
SayedMajid <sydmajid04@gmail.com>
2022-05-04 11:05:48 +0530

    navbar and footer

---
+++ scripts/cities.js
@@ -0,0 +1,4 @@
+import {navbar, footer, addFunctionality} from '../components/navbar.js'
+document.querySelector('#navbar').innerHTML = navbar();
+document.querySelector('#footer').innerHTML = footer();
+addFunctionality();
